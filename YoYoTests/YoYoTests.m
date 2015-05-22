da2d0444baa1ef607ddeda48db9de56031c6f0b0
Junfeng Bai <willie@nichalosiMac.local>
2015-05-22 11:11:06 +0800

    bjf

---
+++ YoYoTests/YoYoTests.m
@@ -0,0 +1,40 @@
+//
+//  YoYoTests.m
+//  YoYoTests
+//
+//  Created by Junfeng Bai on 15/2/9.
+//  Copyright (c) 2015年 yoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface YoYoTests : XCTestCase
+
+@end
+
+@implementation YoYoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
